11d4652c6bf4a3637d90d33dcaf486860ae38b40
junxu <handsomeslow@163.com>
2017-02-25 12:17:04 +0800

    首页改为viewpager

--- app/src/main/java/com/jx/wxhb/activity/MainTabActivity.java
+++ app/src/main/java/com/jx/wxhb/activity/MainTabActivity.java
@@ -1,34 +1,40 @@
 package com.jx.wxhb.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.widget.FrameLayout;
+import android.support.v4.view.ViewPager;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.jx.wxhb.R;
-import com.jx.wxhb.fragment.BaseFragment;
+import com.jx.wxhb.adapter.MainTabAdapter;
 import com.jx.wxhb.fragment.HotNewsFragment;
 import com.jx.wxhb.fragment.LuckMoneyFragment;
 import com.jx.wxhb.fragment.MyFragment;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainTabActivity extends BaseActivity implements BottomNavigationBar.OnTabSelectedListener{
+public class MainTabActivity extends BaseActivity
+        implements ViewPager.OnPageChangeListener{
     LuckMoneyFragment luckMoneyFragment;
     HotNewsFragment hotNewsFragment;
     MyFragment myFragment;
 
-    @Bind(R.id.fragment_wrap)
-    FrameLayout fragmentWrap;
-
     // 底部tab
     @Bind(R.id.bottom_bar)
     BottomNavigationBar bottomBar;
+
+
+    @Bind(R.id.fragment_view_pager)
+    ViewPager fragmentViewPager;
+
+    MainTabAdapter mainTabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,61 +44,42 @@ public class MainTabActivity extends BaseActivity implements BottomNavigationBar
 
         bottomBar.setMode(BottomNavigationBar.MODE_SHIFTING);
         bottomBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
-        bottomBar.addItem(new BottomNavigationItem(R.drawable.cup,"红包")).setActiveColor(R.color.colorPrimary)
-                .addItem(new BottomNavigationItem(R.drawable.contacts,"热点")).setActiveColor(R.color.GreenColor)
-                .addItem(new BottomNavigationItem(R.drawable.eye,"发现")).setActiveColor(R.color.OrangeColor)
-                .addItem(new BottomNavigationItem(R.drawable.settings,"设置")).setActiveColor(R.color.DarkGrayColor)
+        bottomBar.addItem(new BottomNavigationItem(R.drawable.cup, "红包")).setActiveColor(R.color.colorPrimary)
+                .addItem(new BottomNavigationItem(R.drawable.contacts, "热点")).setActiveColor(R.color.GreenColor)
+                .addItem(new BottomNavigationItem(R.drawable.eye, "发现")).setActiveColor(R.color.OrangeColor)
+                .addItem(new BottomNavigationItem(R.drawable.settings, "设置")).setActiveColor(R.color.DarkGrayColor)
                 .setFirstSelectedPosition(0)
                 .initialise();
 
-        setDefaultFragment();
-        bottomBar.setTabSelectedListener(this);
+        mainTabAdapter = new MainTabAdapter(getSupportFragmentManager(), setFragments());
+        fragmentViewPager.setAdapter(mainTabAdapter);
+        fragmentViewPager.addOnPageChangeListener(this);
+
     }
 
-    private void setDefaultFragment() {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.fragment_wrap, LuckMoneyFragment.newInstance("红包大作战"));
-        transaction.commit();
-    }
-
-    @Override
-    public void onTabSelected(int position) {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        switch (position){
-            case 0:
-                if (luckMoneyFragment==null){
-                    luckMoneyFragment = LuckMoneyFragment.newInstance("红包大作战");
-                }
-                ft.replace(R.id.fragment_wrap,luckMoneyFragment);
-                break;
-
-            case 1:
-                if (hotNewsFragment==null){
-                    hotNewsFragment = HotNewsFragment.newInstance("今日微信热点");
-                }
-                ft.replace(R.id.fragment_wrap,hotNewsFragment);
-                break;
-            case 2:
-            case 3:
-                if (myFragment==null){
-                    myFragment = MyFragment.newInstance("设置");
-                }
-                ft.replace(R.id.fragment_wrap,myFragment);
-                break;
-        }
-
-        ft.commitAllowingStateLoss();
+    private List<Fragment> setFragments(){
+        List<Fragment> fragments = new ArrayList<>();
+        luckMoneyFragment = LuckMoneyFragment.newInstance("红包大作战");
+        fragments.add(luckMoneyFragment);
+        hotNewsFragment = HotNewsFragment.newInstance("今日微信热点");
+        fragments.add(hotNewsFragment);
+        myFragment = MyFragment.newInstance("设置");
+        fragments.add(myFragment);
+        return fragments;
     }
 
     @Override
-    public void onTabUnselected(int position) {
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
     }
 
     @Override
-    public void onTabReselected(int position) {
+    public void onPageSelected(int position) {
+        bottomBar.selectTab(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
     }
 }
---
+++ app/src/main/java/com/jx/wxhb/adapter/MainTabAdapter.java
@@ -0,0 +1,32 @@
+package com.jx.wxhb.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by 徐俊 on 2017/2/25.
+ */
+
+public class MainTabAdapter extends FragmentStatePagerAdapter {
+
+    List<Fragment> fragments;
+
+
+    public MainTabAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
