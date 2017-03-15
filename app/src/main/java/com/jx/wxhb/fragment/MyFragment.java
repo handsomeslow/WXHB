b4f3ae02fea8b325cdc75966d0cf3cb6eefa67e1
Jun <handsomeslow@163.com>
2017-03-15 18:56:50 +0800

    add feedback

--- app/src/main/java/com/jx/wxhb/fragment/MyFragment.java
+++ app/src/main/java/com/jx/wxhb/fragment/MyFragment.java
@@ -3,6 +3,7 @@ package com.jx.wxhb.fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -15,6 +16,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.feedback.Comment;
+import com.avos.avoscloud.feedback.FeedbackAgent;
+import com.avos.avoscloud.feedback.FeedbackThread;
 import com.jx.wxhb.BuildConfig;
 import com.jx.wxhb.R;
 import com.jx.wxhb.activity.LoginActivity;
@@ -22,6 +27,7 @@ import com.jx.wxhb.presenter.AdminContract;
 import com.jx.wxhb.presenter.AdminPresenter;
 import com.jx.wxhb.utils.ContentUtil;
 import com.jx.wxhb.utils.ImageLoaderUtil;
+import com.jx.wxhb.utils.SPUtil;
 import com.jx.wxhb.utils.UserUtil;
 import com.liuguangqiang.ipicker.IPicker;
 
@@ -33,6 +39,8 @@ import butterknife.OnClick;
 
 public class MyFragment extends BaseFragment implements View.OnClickListener,
         IPicker.OnSelectedListener, AdminContract.View {
+    private static final String FEEDBACK_COUNT = "feedback_count";
+
     private static final int REQUEST_CODE_LOGIN_SUCCESS = 0X9001;
 
     @Bind(R.id.user_pic_image_view)
@@ -49,9 +57,17 @@ public class MyFragment extends BaseFragment implements View.OnClickListener,
     @Bind(R.id.admin_control_layout)
     LinearLayout adminControlLayout;
 
-    private String title;
+    @Bind(R.id.hb_setting_button)
+    TextView hbSettingButton;
+
+    @Bind(R.id.feedback_button)
+    TextView feedbackButton;
+    @Bind(R.id.feedback_notification_image_view)
+    ImageView feedbackNotificationImageView;
 
     private AdminPresenter adminPresenter;
+
+    private FeedbackAgent agent;
 
     public static MyFragment newInstance(String tag) {
         MyFragment fragment = new MyFragment();
@@ -65,7 +81,6 @@ public class MyFragment extends BaseFragment implements View.OnClickListener,
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            title = getArguments().getString(ContentUtil.EXTRA_ARG_TITLE);
         }
     }
 
@@ -80,6 +95,31 @@ public class MyFragment extends BaseFragment implements View.OnClickListener,
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        agent = new FeedbackAgent(getActivity());
+        agent.getDefaultThread().sync(new FeedbackThread.SyncCallback() {
+            @Override
+            public void onCommentsSend(List<Comment> list, AVException e) {
+                Log.d("jun", "onCommentsSend: " + list.size());
+            }
+
+            @Override
+            public void onCommentsFetch(List<Comment> list, AVException e) {
+                if (e != null) {
+                    e.printStackTrace();
+                    return;
+                }
+
+                if (list != null) {
+                    Log.d("jun", "onCommentsFetch: " + list.size());
+                    int count = SPUtil.getInstance(getActivity()).getInteger(FEEDBACK_COUNT, 0);
+                    if (list.size() > count) {
+                        feedbackNotificationImageView.setVisibility(View.VISIBLE);
+                    }
+                    SPUtil.getInstance(getActivity()).putInteger(FEEDBACK_COUNT, list.size());
+                }
+            }
+        });
+
         adminPresenter = new AdminPresenter(this);
         loginInBt.setOnClickListener(this);
         saveImgBtn.setOnClickListener(this);
@@ -180,6 +220,12 @@ public class MyFragment extends BaseFragment implements View.OnClickListener,
 
     @Override
     public void onPushNewsFinish(int count) {
-        Toast.makeText(getActivity(),"更新了："+count,Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "更新了：" + count, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.feedback_button)
+    public void onFeedbackClick() {
+        feedbackNotificationImageView.setVisibility(View.GONE);
+        agent.startDefaultThreadActivity();
     }
 }
